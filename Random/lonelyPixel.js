72d245664734adbb293d8a5c7e687fb17a0dc94c
Alex Supkay <asupkay1124@gmail.com>
2018-03-20 15:20:20 -0400

    Outline for lonelypixel

--- Random/lonelyPixel.js
+++ Random/lonelyPixel.js
@@ -1,2 +1,10 @@
 /*Lonely Pixel 
 Given an N x M image with black pixels and white pixels, if a pixel is the only one in its color throughout its entire row and column, then it is a lonely pixel. Find the number of lonely pixels in black from the image. (O(NM))*/
+
+let image = [["W", "W"],["B", "B"]]
+
+let lonelyPixel = (image) => {
+
+}
+
+lonelyPixel(image);
