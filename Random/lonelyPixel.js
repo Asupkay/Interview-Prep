af9f711743740bac657c866ea72a0461b6110636
Alex Supkay <asupkay1124@gmail.com>
2018-03-21 20:35:29 -0400

    lonely pixel prototype

--- Random/lonelyPixel.js
+++ Random/lonelyPixel.js
@@ -5,6 +5,38 @@ let image = [["W", "W"],["B", "B"]]
 
 let lonelyPixel = (image) => {
 
+  let lonelypixel = []
+  for(let i = 0; i < image.length; i++) {
+    lonelypixel[i] = [];
+    for(let x = 0; x < image[0].length; x++) {
+      lonelypixel[i][x] = true;
+    }
+  }
+
+  for(let line = 0; line < image.length; line++) {
+    let position = {
+      W: -1,
+      B: -1
+    }
+    for(let i = 0; i < image[0].length; i++) {
+      if(image[line][i] == "W") {
+        if(position.W == -1) {
+          position.W = i;
+        } else {
+          lonelypixel[line][i] = false;
+          lonelypixel[line][position.W] = false;
+        }
+      } else {
+        if(position.B == -1) {
+          position.B = i;
+        } else {
+          lonelypixel[line][i] = false;
+          lonelypixel[line][position.B] = false;
+        }
+      }
+    }   
+  }
+  return lonelypixel;
 }
 
-lonelyPixel(image);
+console.log(lonelyPixel(image));
