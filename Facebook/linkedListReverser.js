613a4d353b6a27b42865c889e411027dc76fa8f6
Alex Supkay <asupkay1124@gmail.com>
2018-08-13 20:03:32 -0400

    More elegant solution for reversing linkedlist

--- Facebook/linkedListReverser.js
+++ Facebook/linkedListReverser.js
@@ -4,24 +4,19 @@ class LinkedList {
   }
 
   reverse() {
-    if(this.head == null || this.head.next == null) {
-      return this.head;
-    } 
-    let prevNode = this.head;
-    let cNode = this.head.next;
-    let nextNode = this.head.next.next;
-    prevNode.next = null;
-    while(cNode != null) {
-      cNode.next = prevNode;
-      prevNode = cNode;
-      cNode = nextNode;
-      if(nextNode != null) {
-        nextNode = nextNode.next;
-      }
+    let currentNode = this.head;
+    let previousNode = null;
+    let nextNode = null
+    while(currentNode) {
+      nextNode = currentNode.next;
+      
+      currentNode.next = previousNode;
+
+      previousNode = currentNode;
+      currentNode = nextNode;
     }
-    this.head = prevNode;
-    console.log(JSON.stringify(this.head));
-    return this.head;
+    this.head = previousNode;
+    return previousNode;
   }
 }
 
@@ -40,4 +35,4 @@ ll.head.next.next.next = new Node(13);
 ll.head.next.next.next.next = new Node(26);
 ll.head.next.next.next.next.next = new Node(32);
 ll.head.next.next.next.next.next.next = new Node(5);
-ll.reverse();
+console.log(JSON.stringify(ll.reverse()));
