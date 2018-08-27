299dfc0d95e3dec1157ee3c0e5e288c109718329
Alex Supkay <asupkay1124@gmail.com>
2018-08-26 20:54:32 -0400

    Implement singleRiffle

---
+++ InterviewCake/3/singleRiffle.js
@@ -0,0 +1,18 @@
+const singleRiffle = (half1, half2, shuffledDeck) => {
+  let half1Index = 0;
+  let half2Index = 0;
+  let shuffledDeckIndex = 0;
+  while(shuffledDeckIndex < shuffledDeck.length) {
+    if(shuffledDeck[shuffledDeckIndex] == half1[half1Index]) {
+      half1Index++;
+    } else if(shuffledDeck[shuffledDeckIndex] == half2[half2Index]) {
+      half2Index++;
+    } else {
+      return false;
+    }
+    shuffledDeckIndex++;
+  }
+  return true;
+}
+
+console.log(singleRiffle([1,2,3], [3,4,5], [1,3,4,2,3,5]));
