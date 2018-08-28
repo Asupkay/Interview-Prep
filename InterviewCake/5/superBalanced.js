64bfa4b90aa90eed114415bc5c9d9f00e2688083
Alex Supkay <asupkay1124@gmail.com>
2018-08-28 17:24:13 -0400

    Implement superbalanced and validBTree

---
+++ InterviewCake/5/superBalanced.js
@@ -0,0 +1,48 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+const superBalanced = (tree) => {
+  const nodes = [];
+  const depths = [];
+
+  nodes.push([tree, 0]);
+
+  while(nodes.length) {
+    let nodePair = nodes.pop();
+    let node = nodePair[0];
+    let depth = nodePair[1];
+
+    if(!node.left && !node.right) {
+      depths.push(depth);
+      if(depths.length > 2 || (depths.length == 2 && Math.abs(depths[0] - depths[1]) > 1)) {
+        return false;
+      }
+    } else {
+      if(node.left) {
+        nodes.push([node.left, depth + 1]);
+      }
+ 
+      if(node.right) {
+        nodes.push([node.right, depth + 1]);
+      }
+    }
+  }
+  return true;
+}
+
+
---
+++ InterviewCake/5/validBTree.js
@@ -0,0 +1,51 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  } 
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+const validBinaryTree = (tree) => {
+  let nodes = [];
+  nodes.push([tree, Math.NEGATIVE_INFINITY, Math.POSITIVE_INFINITY]);
+  while(nodes.length) {
+    let nodePair = nodes.pop();
+    let node = nodePair[0];
+    let lowerBound = nodePair[1];
+    let upperBound = nodePair[2];
+
+    if(node.value > upperBound || node.value < lowerBound) {
+      return false;
+    } else {
+      if(node.left) {
+        nodes.push([node.left, lowerBound, node.value]);
+      }
+
+      if(node.right) {
+        nodes.push([node.right, node.value, upperBound]);
+      }
+    }
+  }
+  return true;
+}
+
+let bTree = new BinaryTreeNode(50);
+let lNode = bTree.insertLeft(30);
+lNode.insertLeft(20);
+lNode.insertRight(60);
+let rNode = bTree.insertRight(80);
+rNode.insertLeft(70);
+rNode.insertRight(90);
+
+console.log(validBinaryTree(bTree));
