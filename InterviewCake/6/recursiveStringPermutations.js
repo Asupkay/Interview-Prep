a9dcd5d41016bcbafd947183f405f9431d53385c
Alex Supkay <asupkay1124@gmail.com>
2018-08-29 16:29:43 -0400

    Other implementation of recursiveStringPerms

--- InterviewCake/6/recursiveStringPermutations.js
+++ InterviewCake/6/recursiveStringPermutations.js
@@ -17,4 +17,22 @@ const stringPermsHelper = (string, letter) => {
   }
 };
 
+const stringPerms2 = (string) => {
+  if(string.length <= 1) {
+    return new Set(string);
+  }
+
+  let firstHalfOfString = string.slice(0, -1);
+  let lastLetter = string[string.length - 1];
+
+  let perms = stringPerms2(firstHalfOfString);
+  let morePerms = [];
+  perms.forEach((perm) => {
+    for(let i = 0; i < perm.length + 1; i++) {
+      morePerms.push(perm.slice(0,i) + lastLetter + perm.slice(i, perm.length));
+    }
+  });
+  return morePerms;
+}
 console.log(stringPerms("cats"));
+console.log(stringPerms2("cats"));
