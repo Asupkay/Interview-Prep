a3b253118060d10b895b7312e0dd394cc3d9dd79
Alex Supkay <asupkay1124@gmail.com>
2018-09-11 22:14:44 -0400

    fix superbalanced and comment up mergeSort

--- Algorithms_DataStructures/mergeSort.js
+++ Algorithms_DataStructures/mergeSort.js
@@ -37,25 +37,31 @@ console.log(mergeSort([6,5,4,3,2,1]));
 console.log(mergeSort([7,6,5,4,3,2,1]));
 
 const mergeSort2 = (array) => {
+  //If the array is of length 1 it is already sorted
   if(array.length === 1) {
     return array;
   }
   
+  //Get the middle of the array and slice it into two along that array
   let middle = Math.floor(array.length/2);
   let left = array.slice(0,middle);
   let right = array.slice(middle, array.length);
 
   return merge(
+    //Merge sort those two halves and the merge sort them again
     mergeSort2(left),
     mergeSort2(right),
   )
 }
 
 merge = (left, right) => {
+  //Make a result array for the complete solution
   let result = [];
+  //Left and right index
   let lIndex = 0;
   let rIndex = 0;
   
+  //Walk down the array depending which one is bigger push it onto results
   while(lIndex < left.length && rIndex < right.length) {
     if(left[lIndex] <= right[rIndex]) {
       result.push(left[lIndex]);
@@ -66,11 +72,13 @@ merge = (left, right) => {
     }
   }
 
+  //Clean up the rest
   while(lIndex < left.length) {
     result.push(left[lIndex]);
     lIndex++;
   }
 
+  //Clean up the rest
   while(rIndex < right.length) {
     result.push(right[rIndex]);
     rIndex++;
--- InterviewCake/5/superBalanced.js
+++ InterviewCake/5/superBalanced.js
@@ -28,9 +28,12 @@ const superBalanced = (tree) => {
     let depth = nodePair[1];
 
     if(!node.left && !node.right) {
-      depths.push(depth);
-      if(depths.length > 2 || (depths.length == 2 && Math.abs(depths[0] - depths[1]) > 1)) {
-        return false;
+      if(depths.indexOf(depth) < 0) {
+        depths.push(depth);
+      
+        if(depths.length > 2 || (depths.length == 2 && Math.abs(depths[0] - depths[1]) > 1)) {
+          return false;
+        }
       }
     } else {
       if(node.left) {
