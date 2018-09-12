36561626b20f34ff66a9eb00b89699dfa9ba4c00
Alex Supkay <asupkay1124@gmail.com>
2018-09-11 22:28:34 -0400

    Commit up super balanced

--- InterviewCake/5/superBalanced.js
+++ InterviewCake/5/superBalanced.js
@@ -17,25 +17,34 @@ class BinaryTreeNode {
 }
 
 const superBalanced = (tree) => {
+  //depth first search stack
   const nodes = [];
+  //List of depth when we find them
   const depths = [];
 
+  //Push on the first node that has a depth of 0
   nodes.push([tree, 0]);
 
+  //While there are still nodes in the stack
   while(nodes.length) {
+    //Pop off our pair and get the node and depth out of it
     let nodePair = nodes.pop();
     let node = nodePair[0];
     let depth = nodePair[1];
 
+    //If the node has no left or right it is a leaf
     if(!node.left && !node.right) {
+      //If the depth is not in the depth array push on the depth
       if(depths.indexOf(depth) < 0) {
         depths.push(depth);
       
+        //If there are more than two depth or if there are two depth and they have a different of more then one return false
         if(depths.length > 2 || (depths.length == 2 && Math.abs(depths[0] - depths[1]) > 1)) {
           return false;
         }
       }
     } else {
+      //Push on the left and right into the stack incrementing depth by one
       if(node.left) {
         nodes.push([node.left, depth + 1]);
       }
@@ -45,6 +54,7 @@ const superBalanced = (tree) => {
       }
     }
   }
+  //If we went through the whole thing without error return true
   return true;
 }
 
