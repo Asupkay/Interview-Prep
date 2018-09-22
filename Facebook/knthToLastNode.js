4074d22783313b9c55d54217260e7a3228e03cb3
Alex Supkay <asupkay1124@gmail.com>
2018-09-22 15:54:42 -0400

    comment knth to last node

--- Facebook/knthToLastNode.js
+++ Facebook/knthToLastNode.js
@@ -6,18 +6,24 @@ class LinkedListNode {
 }
 
 const knthToLast = (numToLast, head) => {
+  //Base node
   let trailNode = head;
+  //Node that sits ahead
   let headNode = head;
   for(let i = 0; i < numToLast; i++) {
+    //If you run into the end return null
     if(!headNode) {
       return null;
     }
+    //Move it forward
     headNode = headNode.next;
   }
+  //While the head node is not at the end
   while(headNode) {
     trailNode = trailNode.next;
     headNode = headNode.next;
   }
+  //Return the follower node
   return trailNode;
 }
 
