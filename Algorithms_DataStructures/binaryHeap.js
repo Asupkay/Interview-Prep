a8874fd42fa263188f993bc37fe829fc7c6e167d
Alex Supkay <asupkay1124@gmail.com>
2018-09-29 19:36:37 -0400

    Comment up binaryheap

--- Algorithms_DataStructures/binaryHeap.js
+++ Algorithms_DataStructures/binaryHeap.js
@@ -1,26 +1,32 @@
 class binaryMinHeap {
   constructor() {
+    //Overall the heap is an array
     this.heap = [];
   } 
 
   addValue(value) {
+    //When you add a value if the array is empty add it to the front
     if(this.heap.length == 0) {
       this.heap.push(value)
     } else {
+      //Put the value onto the end of the heap and save the actual position (not index)
       let position = this.heap.length + 1;
       this.heap.push(value);
+      //While its parents is more than the value and it isn't at the first position
       while(this.heap[Math.floor(position/2) - 1] > value && position > 1) {
         console.log(position);
+        //Put the current value into temp and swap it with its parent
         let temp = this.heap[position - 1];
         this.heap[position - 1] = this.heap[Math.floor(position/2) - 1];
         this.heap[Math.floor(position/2) - 1] = temp;
+        //Switch position to the parent
         position = Math.floor(position/2);
       }
     }
   }
 
   deleteValue(value) {
-    
+    //Find the index of the value that needs to be deleted
     let index = -1;
     for(let i = 0; i < this.heap.length; i++){
       if(this.heap[i] == value) {
@@ -32,9 +38,12 @@ class binaryMinHeap {
       return index;
     }
 
+    //Replace the index with the value at the end of the heap
     this.heap[index] = this.heap.pop();
+    //Same the value of the newest node
     let newNode = this.heap[index];
     let position = index + 1;
+    //While the parent is more than the node move it up
     while(position > 1 && (this.heap[Math.floor(position/2) -1] > newNode)){
       console.log("here1");
       let temp = this.heap[position - 1];
@@ -43,6 +52,7 @@ class binaryMinHeap {
       position = Math.floor(position/2);
     }
 
+    //While the child is less than the current spot switch
     while((this.heap[position*2 - 1] < newNode && this.heap[position*2 - 1] != null) || (this.heap[position*2] < newNode && this.heap[position*2 - 1] != null)) {
       console.log(this.heap);
       console.log("here2 " + position);
